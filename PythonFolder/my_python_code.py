b73eb73d1f899f9779b35105a78c1bc5caef5ee8
MC4615 <mc4615@att.com>
2021-06-21 08:37:28 -0700

    updated documentation

--- PythonFolder/my_python_code.py
+++ PythonFolder/my_python_code.py
@@ -32,4 +32,4 @@ if __name__ == '__main__':
     work_space = 'terraform_cloud_azure_demo'
 
     # get_vars_from_workspace(work_space)
-    create_terraform_cloud_run(work_space, 'azure_run_3')
+    create_terraform_cloud_run(work_space, 'azure_run_4')
