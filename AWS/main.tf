db7a8ed635146f7dc6092ff745943f515a13b6f5
MC4615 <mc4615@att.com>
2021-06-26 08:55:50 -0700

    changed subnet region

--- AWS/main.tf
+++ AWS/main.tf
@@ -12,7 +12,7 @@ resource "aws_vpc" "dev-vpc" {
 resource "aws_subnet" "subnet-1" {
   vpc_id            = aws_vpc.dev-vpc.id
   cidr_block        = var.subnet_cidr
-  availability_zone = "us-east-1a"
+  availability_zone = "us-west-1a"
 
   tags = {
     Name = var.subnet_tag
