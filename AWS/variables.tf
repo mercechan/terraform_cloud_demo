dd3f259fc2ceed4324df08a5ed21a6ec96efbe6b
Merce Chan <merce_chan@yahoo.com>
2022-03-04 22:15:04 -0800

    Update variables.tf
    cleaned up variables

--- AWS/variables.tf
+++ AWS/variables.tf
@@ -1,13 +1,3 @@
-variable "subnet_cidr" {
-  description = "cidr block for the subnet"
-  type = string
-}
-
-variable "subnet_tag" {
-  description = "tag block for the subnet"
-  type = string
-}
-
 variable "aws_region" {
   type    = string
   default = "us-west-1"
