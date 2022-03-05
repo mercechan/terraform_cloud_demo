5ead786883d856c5766732c6f8b5e3c525c0818e
Merce Chan <merce_chan@yahoo.com>
2022-03-04 22:14:32 -0800

    Update main.tf
    expanded deployment config

--- AWS/main.tf
+++ AWS/main.tf
@@ -2,19 +2,29 @@ provider "aws" {
   region = var.aws_region
 }
 
-resource "aws_vpc" "dev-vpc" {
-  cidr_block = "10.0.0.0/16"
+#Create your webserver instance
+resource "aws_instance" "Web" {
+  ami           = "ami-078f16034794ecfbd"
+  instance_type = "t2.micro"
   tags = {
-    Name = "development"
+    Name = "Node-Red-Server"
+  }
+  count           = 1
+  key_name        = "generic-ssh-key"
+  subnet_id = aws_subnet.node-red-vpc-subnet-public1-us-west-1a.id 
+  vpc_security_group_ids = [aws_security_group.nodered-sg.id]
+
+  provisioner "remote-exec" {
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = file("${path.module}/keyfiles/generic-ssh-key.pem")
+      host        = aws_instance.Web[0].public_ip
+    }
+
+    inline = [
+      "docker run -d -p 80:1880 -v node_red_data:/data --name mynodered nodered/node-red:latest"
+    ]
   }
 }
 
-resource "aws_subnet" "subnet-1" {
-  vpc_id            = aws_vpc.dev-vpc.id
-  cidr_block        = var.subnet_cidr
-  availability_zone = "us-west-1a"
-
-  tags = {
-    Name = var.subnet_tag
-  }
-}
