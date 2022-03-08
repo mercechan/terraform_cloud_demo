8e6c7d68b2a28fdd646b758d7a613ba60811c76a
Merce Chan <merce_chan@yahoo.com>
2022-03-08 09:28:24 -0800

    Update security.tf

--- AWS/security.tf
+++ AWS/security.tf
@@ -15,6 +15,7 @@ resource "aws_subnet" "node-red-vpc-subnet-public1-us-west-1a" {
   vpc_id                  = aws_vpc.node-red-vpc-vpc.id
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
+  enable_resource_name_dns_a_record_on_launch = true
   depends_on              = [aws_vpc.node-red-vpc-vpc]
 }
 
